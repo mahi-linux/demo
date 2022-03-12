8d3fe4eb5e0b55f48a0018df088fa59433f104e9
Mahesh <mahi.linux@gmail.com>
2022-03-12 13:23:45 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script is used to upload the custom-root-ca
+# Created by Mahesh
+# version 1.0
+
+file='${workspace}/custom-root-ca'
+
+ls -altr $file
+ls -altr $file/custom-root-ca
+
+else
+echo "file not found"
+exit 1
+fi
